d63873cdd583a9fc37575c3d46a8d4fe13dd3ac8
Setysen <setysen@mail.ru>
2018-09-26 12:03:39 +0300

    Test

--- test/test_tbitfield.cpp
+++ test/test_tbitfield.cpp
@@ -7,6 +7,10 @@ TEST(TBitField, can_create_bitfield_with_positive_length)
   ASSERT_NO_THROW(TBitField bf(3));
 }
 
+TEST(TEST, test){
+	EXPECT_EQ(0, 0);
+}
+
 TEST(TBitField, can_get_length)
 {
   TBitField bf(3);
